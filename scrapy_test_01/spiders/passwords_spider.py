4ef9d83dd5c24bb766dd8e91d2ea44edd3f9af85
hbp_02 <hector.bolsa@outlook.com>
2019-01-16 07:40:26 +0000

    More spiders...

--- scrapy_test_01/spiders/passwords_spider.py
+++ scrapy_test_01/spiders/passwords_spider.py
@@ -1,15 +1,15 @@
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
-    name = "proxies"
+    name = "passwords"
 
     def start_requests(self):
         start_urls = [
-            'http://quotes.toscrape.com/page/1/',
+            'https://foro.putalocura.com/threads/hilo-de-passwords-2-0.77820/page-2',
         ]
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'proxies-%s.html' % page
+        filename = 'passwords-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
--- scrapy_test_01/spiders/quotes_spider.py
+++ scrapy_test_01/spiders/quotes_spider.py
@@ -5,7 +5,6 @@ class QuotesSpider(scrapy.Spider):
 
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
